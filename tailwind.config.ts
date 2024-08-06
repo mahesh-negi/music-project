2d355bd5ed1ff18eebb54b8b060e562d88b3dd0d
mahesh-negi <mnegi231999@gmail.com>
2024-08-06 13:47:55 +0530

    changes in herosection page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,6 +33,19 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+  ],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 export default config;
